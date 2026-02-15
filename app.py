a61219a4510a43a8825c9c94a4aac04fa10bf71f
anapelko <ana.pelko3@gmail.com>
2026-02-15 23:44:30 +0100

    dodan kontroler za autentifikaciju i registraciju te prepravljeni login.html, app.py i style.css

--- app.py
+++ app.py
@@ -1,70 +1,19 @@
-from flask import Flask, render_template, request, redirect, session;
-from flask_session import Session;
-import pymysql.cursors;
-from werkzeug.security import generate_password_hash, check_password_hash;
-import secrets;
+from flask import Flask
+from config import Config
+from database import db
 
+from controllers.auth import auth_bp
+from controllers.lobby import lobby_bp
 
-# KONFIGURACIJA --------
+app = Flask(__name__)
+app.config.from_object(Config)
 
-app = Flask(__name__);
-app.secret_key = secrets.token_hex(16);
+# Inicijalizacija baze
+db.init_app(app)
 
-app.config['SESSION_TYPE'] = 'cachelib';
-app.config['SESSION_PERMANENT'] = False;
+# Registracija blueprintova
+app.register_blueprint(auth_bp)
+app.register_blueprint(lobby_bp)
 
-Session(app);
-
-def get_db_connection():
-    return pymysql.connect(
-        host='rwa.studenti.math.hr',
-        user='apelko',
-        password='1191250989',
-        database='apelko_bela',
-        cursorclass=pymysql.cursors.DictCursor
-    );
-
-# RUTE --------
-
-@app.route('/', methods=['GET','POST'])
-def index():
-    return render_template('login.html');
-
-@app.route('/login', methods=['GET','POST'])
-def checklogin():
-    username = request.form.get('username');
-    password = request.form.get('password');
-    conn = get_db_connection();
-    cursor = conn.cursor();
-    cursor.execute("SELECT password_hash, has_registered FROM users WHERE username = %s",(username,));
-    user=cursor.fetchone();
-    cursor.close();
-    conn.close();
-
-    # PROVJERI JESMO LI NAŠLI USER-a S TIM NICKOM
-    if(user):
-
-        # AKO POSTOJI USER, ONDA PROVJERI ŠIFRU
-        if(not(check_password_hash(user['password_hash'],password))):
-            # AKO SMO UŠLI U OVAJ IF ZNAČI DA SU ILI USERNAME ILI LOZINKA NETOČNI
-            return redirect('/');
-    
-        if user['has_registered'] != 1:
-            # AKO SMO UŠLI U OVAJ IF ZNAČI DA KORISNIK NIJE DOVRŠIO REGISTRACIJU, KASNIJE ĆEMO TO DODATI
-            return redirect('/');
-    
-        # AKO SMO OVO PREZIVJELI LOGIN JE DOBAR I POSTAVIMO SESSION VARIJABLE I SALJEMO KORISNIKA U LOBBY
-        session['username'] = username;
-        return redirect('/lobby');
-
-    # AKO SMO DOŠLI NEKIM SLUČAJEM DO KRAJA, A NEMAMO USERA ILI NISMO PRONAŠLI USERA ONDA JAVI GREŠKU I VRATI
-    # KORISNIKA NA LOGIN PONOVNO
-    if 'username' not in session:
-        return redirect('/');
-
-@app.route('/lobby')
-def lobby():
-    #AKO NETKO DODE U LOBBY BEZ LOGINA VRACAMO GA NATRAG NA LOGIN STRANICU
-    if 'username' not in session:
-        return redirect('/');
-    return render_template('lobby.html');
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ controllers/auth.py
@@ -0,0 +1,96 @@
+from flask import Blueprint, render_template, redirect, url_for, request, session, flash
+from database import db
+from models.igrac import Igrac
+
+auth_bp = Blueprint('auth', __name__)
+
+
+# RUTA ZA PRIKAZ LOGIN STRANICE
+@auth_bp.route('/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+
+#LOGIN RUTA, PROVJERA KORISNIKA I POSTAVLJANJE SESSION-A
+@auth_bp.route('/login', methods=['POST'])
+def checklogin():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        flash("Neispravno korisničko ime ili lozinka.")
+        return redirect(url_for('auth.index'))
+
+    korisnik = Igrac.query.filter_by(username=username).first()
+
+    # PROVJERI JESMO LI NAŠLI USER-a S TIM IMENOM
+    if korisnik:
+
+        # AKO POSTOJI USER, ONDA PROVJERI ŠIFRU
+        if not korisnik.check_password(password):
+            flash("Neispravno korisničko ime ili lozinka.")
+            return redirect(url_for('auth.index'))
+
+        # AKO SMO OVO PREŽIVJELI LOGIN JE DOBAR, PROVJERI JEL REGISTRIRAN
+        if not korisnik.jel_registriran:
+            flash("Korisnik nije dovršio registraciju.")
+            return redirect(url_for('auth.index'))
+
+        # POSTAVI SESSION AKO JE SVE PRIJE PROSLO
+        session['id_igraca'] = korisnik.id_igraca
+        session['username'] = korisnik.username
+
+        # SVE JE DOBRO, PREUSMJERI KORISNIKA NA LOBBY
+        flash("Uspješno ste prijavljeni!")
+        return redirect(url_for('lobby.prikaz_lobbyja'))
+
+
+    # AKO USER NE POSTOJI
+    flash("Korisnik ne postoji.")
+    return redirect(url_for('auth.index'))
+
+
+# RUTA ZA REGISTRACIJU, PROVJERA UNOSA I KREIRANJE NOVOG KORISNIKA U BAZI
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    mail = request.form.get('mail')
+
+    # PROVJERA DA LI SU SVA POLJA POPUNJENA
+    if not username or not password or not mail:
+        flash("Sva polja su obavezna.")
+        return redirect(url_for('auth.index'))
+
+    # PROVJERA POSTOJI LI USER
+    postoji = Igrac.query.filter_by(username=username).first()
+    if postoji:
+        flash("Korisničko ime već postoji.")
+        return redirect(url_for('auth.index'))
+
+    # SVE JE DOBRO, KREIRAJ NOVOG KORISNIKA
+    novi_igrac = Igrac(
+        username=username,
+        mail=mail,
+        jel_registriran=True,
+        br_odigranih=0,
+        br_pobjeda=0
+    )
+
+    # HASHIRANJE LOZINKE
+    novi_igrac.set_password(password)
+
+    # SPREMI KORISNIKA U BAZU
+    db.session.add(novi_igrac)
+    db.session.commit()
+
+    flash("Registracija uspješna!")
+    return redirect(url_for('auth.index'))
+
+
+# RUTA ZA ODJAVU, ČIŠĆENJE SESSION-A I PREUSMJERAVANJE NA LOGIN
+@auth_bp.route('/logout')
+def logout():
+    session.clear()
+    flash("Odjavljeni ste.")
+    return redirect(url_for('auth.login'))
