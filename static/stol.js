6c4841e9db95af3e94c0fc663ba7c6fbd28c0759
anapelko <ana.pelko3@gmail.com>
2026-02-23 02:42:03 +0100

    popravci u stol.js

--- static/stol.js
+++ static/stol.js
@@ -19,10 +19,26 @@ function ucitajStanje() {
             }
 
             // OSNOVNE INFORMACIJE
-            document.getElementById("adut").innerText = data.adut || "-";
-            document.getElementById("na-redu").innerText = data.na_redu;
+            const adutMap = {
+            "H": "Herc",
+            "K": "Karo",
+            "P": "Pik",
+            "T": "Tref"
+            };
 
-
+            if (data.adut && adutMap[data.adut]) {
+                document.getElementById("adut").innerText = adutMap[data.adut];
+            } else {
+                document.getElementById("adut").innerText = "-";
+            }
+            
+            const imena = data.imena_igraca;
+            if (imena && imena[data.na_redu]) {
+                document.getElementById("na-redu").innerText = imena[data.na_redu];
+            } else {
+                document.getElementById("na-redu").innerText = data.na_redu;
+            }
+            
             //REZULTAT RUNDE (bez zvanja)
             document.getElementById("mi-runda").innerText = data.rezultat_runde.mi;
             document.getElementById("vi-runda").innerText = data.rezultat_runde.vi;
@@ -42,12 +58,38 @@ function ucitajStanje() {
 
             const mojId = data.id_ovog_igraca;
             const red = data.red_igranja;
-            const karteNaStolu = data.stol;
+            const karteNaStolu = data.stol;           
 
             // očisti sve pozicije
             ["dolje", "lijevo", "gore", "desno"].forEach(poz => {
                 document.getElementById(poz).innerHTML = "";
             });
+
+            // sada dodaj imena
+            if(imena){
+                Object.keys(imena).forEach(logickiId => {
+
+                    const relativna = (parseInt(logickiId) - mojId + 4) % 4;
+
+                    let pozicija = "";
+                    if (relativna === 0) pozicija = "dolje";
+                    if (relativna === 1) pozicija = "lijevo";
+                    if (relativna === 2) pozicija = "gore";
+                    if (relativna === 3) pozicija = "desno";
+
+                    // ako je to moj igrač, ne prikazujemo ime
+                    if (relativna !== 0) {
+
+                        const imeDiv = document.createElement("div");
+                        imeDiv.innerText = imena[logickiId];
+                        imeDiv.className = "ime-igraca";
+                        imeDiv.style.fontWeight = "bold";
+                        imeDiv.style.marginBottom = "5px";
+
+                        document.getElementById(pozicija).prepend(imeDiv);
+                    }
+                });
+            }
 
             // prolazimo kroz karte redom bacanja
             for (let i = 0; i < karteNaStolu.length; i++) {
